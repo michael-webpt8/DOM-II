22e7f6ecc500067f2763e48f975fb32136394c77
Michael Williams <manicdesigner@gmail.com>
2019-08-08 12:17:22 -0400

    feat(index.js) + click event 1 logoHeading changes fontSize & color

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// number 1. click event on heading.
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('click', event => {
+  event.target.style.fontSize = '4.5rem';
+  event.target.style.color = 'dodgerblue';
+});
