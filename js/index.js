6daf29aedc2ecead9ed7f62d2d07fbf0440e55ff
Michael Williams <manicdesigner@gmail.com>
2019-08-09 12:37:08 -0400

    feat(index.js) + load - window color h2 intro & h2 .text-content dodgerblue

--- js/index.js
+++ js/index.js
@@ -4,17 +4,29 @@
  * number 1. click event on heading.
  */
 const logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener('click', event => {
-  event.target.style.fontSize = '4.5rem';
-  event.target.style.color = 'dodgerblue';
+logoHeading.addEventListener('click', (event) => {
+	event.target.style.fontSize = '4.5rem';
+	event.target.style.color = 'dodgerblue';
+});
+
+/**
+ * 2. on load event nav turns blue
+ */
+window.addEventListener('load', (event) => {
+	const introHeading = document.querySelector('.intro h2');
+	const headingsOnLoad = document.querySelectorAll('.text-content h2');
+	introHeading.style.color = 'dodgerblue';
+	headingsOnLoad.forEach((heading) => {
+		heading.style.color = 'dodgerblue';
+	});
 });
 
 /**
  * prevent nav from refreshing.
  */
 const navPreventRefresh = document.querySelectorAll('.nav-link');
-navPreventRefresh.forEach(navLinks => {
-  navLinks.addEventListener('click', event => {
-    event.preventDefault();
-  });
+navPreventRefresh.forEach((navLinks) => {
+	navLinks.addEventListener('click', (event) => {
+		event.preventDefault();
+	});
 });
