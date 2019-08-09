f2330a2af80fa87e6ff4155bb3a141560f4a27e9
Michael Williams <manicdesigner@gmail.com>
2019-08-09 09:19:59 -0400

    feat(index.js) prevent nav from refreshing page

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 // Your code goes here
 
-// number 1. click event on heading.
+/**
+ * number 1. click event on heading.
+ */
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('click', event => {
   event.target.style.fontSize = '4.5rem';
   event.target.style.color = 'dodgerblue';
+});
+
+/**
+ * prevent nav from refreshing.
+ */
+const navPreventRefresh = document.querySelectorAll('.nav-link');
+navPreventRefresh.forEach(navLinks => {
+  navLinks.addEventListener('click', event => {
+    event.preventDefault();
+  });
 });
