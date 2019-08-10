1887c1fae0e623a284c0bd6d4d54630a5ecec8b0
Michael Williams <manicdesigner@gmail.com>
2019-08-10 11:08:29 -0400

    add keypress event on window,  so available right away.

--- js/index.js
+++ js/index.js
@@ -4,47 +4,66 @@
  * number 1. click event on heading.
  */
 const logoHeading = document.querySelector('.logo-heading');
-logoHeading.addEventListener('click', (event) => {
-	event.target.style.fontSize = '4.5rem';
-	event.target.style.color = 'dodgerblue';
+logoHeading.addEventListener('click', event => {
+  event.target.style.fontSize = '4.5rem';
+  event.target.style.color = 'dodgerblue';
 });
 
 /**
  * 2. on load event nav turns blue
  */
-window.addEventListener('load', (event) => {
-	const introHeading = document.querySelector('.intro h2');
-	const headingsOnLoad = document.querySelectorAll('.text-content h2');
-	introHeading.style.color = 'dodgerblue';
-	headingsOnLoad.forEach((heading) => {
-		heading.style.color = 'dodgerblue';
-	});
+window.addEventListener('load', event => {
+  const introHeading = document.querySelector('.intro h2');
+  const headingsOnLoad = document.querySelectorAll('.text-content h2');
+  introHeading.style.color = 'dodgerblue';
+  headingsOnLoad.forEach(heading => {
+    heading.style.color = 'dodgerblue';
+  });
 });
 
 /**
  *  3. dblclick event
  */
 const funBus = document.querySelector('#bus');
-funBus.addEventListener('dblclick', (event) => {
-	const funBusImg = document.querySelector('img#bus');
-	funBusImg.classList.toggle('fun-bus');
+funBus.addEventListener('dblclick', event => {
+  const funBusImg = document.querySelector('img#bus');
+  funBusImg.classList.toggle('fun-bus');
 });
 
 /**
  * 4. mousedown event
  */
 const destinationImg = document.querySelector('.content-destination img');
-destinationImg.addEventListener('mousedown', (event) => {
-	event.currentTarget.style.transition = 'all .3s ease-in-out';
-	event.currentTarget.style.transform = 'scale(.5)';
+destinationImg.addEventListener('mousedown', event => {
+  event.currentTarget.style.transition = 'all .3s ease-in-out';
+  event.currentTarget.style.transform = 'scale(.5)';
 });
 
 /**
  * prevent nav from refreshing.
  */
 const navPreventRefresh = document.querySelectorAll('.nav-link');
-navPreventRefresh.forEach((navLinks) => {
-	navLinks.addEventListener('click', (event) => {
-		event.preventDefault();
-	});
+navPreventRefresh.forEach(navLinks => {
+  navLinks.addEventListener('click', event => {
+    event.preventDefault();
+  });
+});
+
+window.addEventListener('keypress', event => {
+  let key = event.key;
+  let navItem = document.querySelector('.nav-link');
+  switch (key) {
+    case 'h':
+      navItem = document.querySelectorAll('.nav-link')[0];
+      break;
+    case 'a':
+      navItem = document.querySelectorAll('.nav-link')[1];
+      break;
+    case 'b':
+      navItem = document.querySelectorAll('.nav-link')[2];
+      break;
+    case 'c':
+      navItem = document.querySelectorAll('.nav-link')[3];
+      break;
+  }
 });
