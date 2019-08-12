8a231979e14f87cb252612d1bac3f22fe7c81a7d
Michael Williams <manicdesigner@gmail.com>
2019-08-12 15:29:43 -0400

    messing with drag drop ...drags text from one spot puts to another

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ destinationImg.addEventListener('mousedown', event => {
 });
 
 /**
- *  5. key up event NOT WORKING.
+ *  5. key up event NOT WORKING CORRECTLY.
  */
 window.addEventListener('keyup', event => {
   let key = event.key;
@@ -97,6 +97,91 @@ navs.forEach(links => {
   links.addEventListener('mouseout', event => {
     event.target.style.color = '#212529';
   });
+});
+
+const pTags = document.querySelectorAll('p');
+const pars = document.querySelectorAll('p');
+pars.forEach(par => {
+  par.classList.add('dropzone');
+});
+
+pTags.forEach(p => {
+  let dragged;
+  /* events fired on the draggable target */
+  document.addEventListener(
+    'drag',
+    function(event) {
+      event.target.draggable = true;
+    },
+    false
+  );
+
+  p.addEventListener(
+    'dragstart',
+    function(event) {
+      // store a ref. on the dragged elem
+      dragged = event.target;
+      // make it half transparent
+      event.target.style.opacity = 0.5;
+    },
+    false
+  );
+
+  document.addEventListener(
+    'dragend',
+    function(event) {
+      // reset the transparency
+      event.target.style.opacity = '';
+    },
+    false
+  );
+
+  /* events fired on the drop targets */
+  document.addEventListener(
+    'dragover',
+    function(event) {
+      // prevent default to allow drop
+      event.preventDefault();
+    },
+    false
+  );
+
+  document.addEventListener(
+    'dragenter',
+    function(event) {
+      // highlight potential drop target when the draggable element enters it
+      if (event.target.className === 'drop-area') {
+        event.target.style.backgroundColor = 'white';
+      }
+    },
+    false
+  );
+
+  document.addEventListener(
+    'dragleave',
+    function(event) {
+      // reset background of potential drop target when the draggable element leaves it
+      if (event.target.className == 'dropzone') {
+        event.target.style.background = '';
+      }
+    },
+    false
+  );
+
+  document.addEventListener(
+    'drop',
+    function(event) {
+      // prevent default action (open as link for some elements)
+      event.preventDefault();
+      // move dragged elem to the selected drop target
+      if (event.target.className == 'dropzone') {
+        event.target.style.background = '';
+        dragged.parentNode.removeChild(dragged);
+        event.target.appendChild(dragged);
+      }
+    },
+    false
+  );
 });
 
 /**
